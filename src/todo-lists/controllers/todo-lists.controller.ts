95c0e5bae97ec2b994addff89f5640bd189fcc91
Lindell Carter <lindellcarternyc@gmail.com>
2021-06-03 14:47:48 -0400

    implement added functionality

--- src/todo-lists/controllers/todo-lists.controller.ts
+++ src/todo-lists/controllers/todo-lists.controller.ts
@@ -4,6 +4,9 @@ import { TodoListService, TodoListServiceImpl } from '../services/todo-lists.ser
 
 export interface TodoListController {
   createList: ExpressFunc
+  getAllLists: ExpressFunc
+  deleteAllLists: ExpressFunc
+  createTodo: ExpressFunc
 }
 
 @injectable()
@@ -14,11 +17,49 @@ export class TodoListControllerImpl implements TodoListController {
     const userId = res.locals.jwt.id
     const todos = req.body.todos as Array<{task : string}>
     
-    const newList = await this.todoListService.createTodo({
+    const newList = await this.todoListService.createTodoList({
       userId,
       todos
     })
 
     return res.status(201).send(newList)
+  }
+
+  getAllLists: ExpressFunc = async (req, res) => {
+    try {
+      const todoLists = await this.todoListService.getAllLists()
+      return res.status(200).send({ todoLists })
+    } catch (err) {
+      throw err
+    }
+  }
+
+  createTodo: ExpressFunc = async (req, res) => {
+    try {
+      const listId = parseInt(req.params.listId)
+      
+      const newTodo = await this.todoListService.createTodo({
+        listId,
+        todo: {
+          task: req.body.task
+        }
+      })
+
+      return res.status(201).send({ todo: newTodo })
+    } catch (err) {
+      return res.status(500).send('INTERNAL SERVER ERROR')
+    }
+  }
+
+  deleteAllLists: ExpressFunc = async (req, res) => {
+    try {
+      await this.todoListService.deleteAllTodoLists()
+      return res.status(203).send('DELETED ALL TODO LISTS')
+    } catch (err) {
+      return res.status(500).send({
+        message: 'INTERNAL SERVER ERROR',
+        err
+      })
+    }
   }
 }
