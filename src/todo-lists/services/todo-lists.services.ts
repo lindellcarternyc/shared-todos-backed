552acf4e3175a735c5470b28fe8e4575c919f3ba
Lindell Carter <lindellcarternyc@gmail.com>
2021-06-03 14:46:37 -0400

    cleanup types in todo lists services

--- src/todo-lists/services/todo-lists.services.ts
+++ src/todo-lists/services/todo-lists.services.ts
@@ -1,4 +1,4 @@
-import { TodoList } from '@prisma/client'
+import { Todo, TodoList } from '@prisma/client'
 import { inject, injectable } from 'inversify';
 import { PrismaService, PrismaServiceImpl } from '../../common/services/primsa.service';
 
@@ -7,15 +7,23 @@ interface CreateTodoList {
   todos: Array<{ task: string }>
 }
 
+interface CreateTodo {
+  listId: number
+  todo: { task: string }
+}
 export interface TodoListService {
-  createTodo(args: CreateTodoList): Promise<TodoList>
+  createTodoList(args: CreateTodoList): Promise<TodoList>
+  createTodo(args: CreateTodo): Promise<Todo>
+  getAllLists(): Promise<TodoList[]>
+  getListById(id: number): Promise<TodoList | null>
+  deleteAllTodoLists(): Promise<void>
 }
 
 @injectable()
 export class TodoListServiceImpl implements TodoListService {
   constructor(@inject(PrismaServiceImpl) private readonly db: PrismaService) { }
 
-  createTodo: TodoListService['createTodo'] = async (args) => {
+  createTodoList: TodoListService['createTodoList'] = async (args) => {
     try {
       const emptyList = await this.db.connection.todoList.create({
         data: {
@@ -26,7 +34,7 @@ export class TodoListServiceImpl implements TodoListService {
       })
 
       for (const todo of args.todos) {
-        const newTodo = await this.db.connection.todo.createMany({
+       await this.db.connection.todo.createMany({
           data: {
             task: todo.task,
             listId: emptyList.id
@@ -45,6 +53,50 @@ export class TodoListServiceImpl implements TodoListService {
         throw new Error('Error creating list')
       }
       return newList
+    } catch (err) {
+      throw err
+    }
+  }
+
+  getAllLists = async () => {
+    const lists = await this.db.connection.todoList.findMany(
+      { 
+        include: {
+          todos: true
+        }
+      }
+    )
+    return lists
+  }
+  
+  getListById = async (id: number) => {
+    return this.db.connection.todoList.findFirst({
+      where: { id }
+    })
+  }
+
+  createTodo = async (args: CreateTodo) => {
+    const { listId, todo } = args
+    try {
+      if (await this.getListById(listId)) {
+        const newTodo = await this.db.connection.todo.create({
+          data: {
+            task: todo.task,
+            listId
+          }
+        })
+        return newTodo
+      }
+      throw new Error(`Could not find list ${listId}`)
+    } catch (err) {
+      throw err
+    }
+  }
+
+  deleteAllTodoLists = async () => {
+    try {
+      await this.db.connection.todo.deleteMany()
+      await this.db.connection.todoList.deleteMany()
     } catch (err) {
       throw err
     }
