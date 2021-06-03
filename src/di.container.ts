342f36cd778f2de4a7af191c1df994a4135c252b
Lindell Carter <lindellcarternyc@gmail.com>
2021-06-03 14:42:08 -0400

    add comments for clarification

--- src/di.container.ts
+++ src/di.container.ts
@@ -8,18 +8,25 @@ import { UsersController, UsersControllerImpl } from './users/users.controller'
 import dotenv from 'dotenv'
 import { TodoListController, TodoListControllerImpl } from './todo-lists/controllers/todo-lists.controller'
 import { TodoListService, TodoListServiceImpl } from './todo-lists/services/todo-lists.services'
+import { UsersMiddleware, UsersMiddlewareImpl } from './users/middleware/users.middleware'
 dotenv.config()
 
 const container = new Container()
 
+// DATABASE
 container.bind<PrismaService>(PrismaServiceImpl).toSelf()
 
+// USERS
+container.bind<UsersMiddleware>(UsersMiddlewareImpl).toSelf()
 container.bind<UsersController>(UsersControllerImpl).toSelf()
 container.bind<UserService>(UserServiceImpl).toSelf()
 
+// AUTH
 container.bind<AuthController>(AuthControllerImpl).toSelf()
 container.bind<AuthService>(AuthServiceImpl).toSelf()
 
+
+// LISTS
 container.bind<TodoListController>(TodoListControllerImpl).toSelf()
 container.bind<TodoListService>(TodoListServiceImpl).toSelf()
 
