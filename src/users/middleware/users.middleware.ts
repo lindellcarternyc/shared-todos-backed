2c9d0a7b82c50a5e10ec05ff975126f7ebba77d0
Lindell Carter <lindellcarternyc@gmail.com>
2021-06-07 17:28:43 -0400

    adds middleware for user ownership of list

--- src/users/middleware/users.middleware.ts
+++ src/users/middleware/users.middleware.ts
@@ -1,9 +1,11 @@
+import { User } from '@prisma/client'
 import { inject, injectable } from 'inversify'
 import { ExpressFunc } from '../../common/interfaces'
 import { UserService, UserServiceImpl } from '../services/users.service'
 
 export interface UsersMiddleware {
   userExists: ExpressFunc
+  userOwnsList: ExpressFunc
 }
 
 @injectable()
@@ -21,5 +23,23 @@ export class UsersMiddlewareImpl implements UsersMiddleware {
     }
 
     return res.status(404).send('NO USER FOUND')
+  }
+
+  userOwnsList: ExpressFunc = async (req, res, next) => {
+    const userId = res.locals.jwt.id
+
+    const user = await this.usersService.getUserById(userId)
+
+    const listId = parseInt(req.params.listId)
+
+    if (user) {
+      for (const list of user.createdLists) {
+        if (list.id === listId) {
+          return next()
+        }
+      }
+    }
+
+    return res.status(403).send('FORBIDDEN')
   }
 }
